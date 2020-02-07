0bd32e7f075ca9790121427f9fc04f5ca4115cc8
Udeste <udeste@gmail.com>
2020-02-07 20:15:08 +0100

    Update settins.h

--- include/settings.h
+++ include/settings.h
@@ -1,9 +1,5 @@
 #include "Arduino.h"
 #include "SoftwareSerial.h"
-#undef F
-#include <common/mavlink.h>
-#include <mavlink_types.h>
-#include <ardupilotmega/ardupilotmega.h>
 
 //  extern "C" {
 //     // Espressif SDK
@@ -18,31 +14,31 @@
 #define HAL_GPIO_C_LED_PIN          30
 #define HEARTHBEAT_TO_FC_LED_PIN    HAL_GPIO_C_LED_PIN
 #define HEARTHBEAT_FROM_FC_LED_PIN  HAL_GPIO_A_LED_PIN
+#define FRSKY_LED_PIN               HAL_GPIO_B_LED_PIN
 
 /**
  * FRSKY communication settings
  */
-#define FRSKY_SWSERIAL_RX_PIN       12
-#define FRSKY_SWSERIAL_TX_PIN       14
+#define FRSKY_SWSERIAL_RX_PIN       A8
+#define FRSKY_SWSERIAL_TX_PIN       A9
 #define FRSKY_SWSERIAL_BAUD         57600
-#define FRSKY_LED_PIN               LED_BUILTIN
 
 /**
  * FLIGHT CONTROLLER commuication settings
  */
-#define FC_SERIAL                   Serial1
-#define FC_SERIAL_BAUD              115200
+#define FC_SERIAL                   Serial2
+#define FC_SERIAL_BAUD              57600
 
 /**
  * BRIDGE mavlink configuration for this bridge
  */
-#define HEARTBEAT_TO_FC_MS          2000
+#define HEARTBEAT_TO_FC_MS          1000
 #define BRIDGE_SYSTEM_ID            9
 #define BRIDGE_COMPONENT_ID         1
-#define BRIDGE_TYPE                 MAV_TYPE_GCS
-#define BRIDGE_AUTOPILOT            MAV_AUTOPILOT_ARDUPILOTMEGA
+#define BRIDGE_TYPE                 MAV_TYPE_GCS                // https://mavlink.io/en/messages/common.html#MAV_TYPE
+#define BRIDGE_AUTOPILOT            MAV_AUTOPILOT_ARDUPILOTMEGA // https://mavlink.io/en/messages/common.html#MAV_AUTOPILOT
 #define BRIDGE_BASE_MODE            0
-#define BRIDGE_SYSTEM_STATUS        MAV_STATE_ACTIVE
+#define BRIDGE_SYSTEM_STATE         MAV_STATE_ACTIVE             // https://mavlink.io/en/messages/common.html#MAV_STATE
 
 /**
  * WIFI config section
@@ -52,4 +48,4 @@
 #define WIFI_CHAN                   9
 
 #define DEBUG                       Serial
-#define DEBUG_BAUD                  115200
+#define DEBUG_BAUD                  57600
